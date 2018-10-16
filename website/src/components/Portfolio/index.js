b4304f3111600ce4565aec37d8c49abad9e53323
jonathan08 <jonathan@cosmunsoftwares.com.br>
2018-10-16 13:47:01 -0300

    pending effect show detail project

--- website/src/components/Portfolio/index.js
+++ website/src/components/Portfolio/index.js
@@ -1,24 +1,56 @@
 import React from 'react'
 import './styles.scss'
-import { translate } from 'react-i18next'
+import { translate, Trans } from 'react-i18next'
 
 import Title from '../Title'
 import Button from '../Button'
 
 class Item extends React.Component {
+
+  state = {
+    active: false,
+  }
+
+  open_project = () => {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+
   render(){
     let type = this.props.value.type
     if (this.props.value.type === 'mobile') {type = 'aplicativo'}
     else if(this.props.value.type === 'mobile'){type = 'sistema'}
 
     return (
-      <div className="col-md-4 col-sm-6 col-12 mb-4">
-        <div className="project position-relative">
-          <img src={ this.props.value.featured_image.url } className="w-100" alt={ this.props.value.name } title={ this.props.value.name } />
-          <div className="info text-light px-4 py-2">
-            <i className="fas fa-desktop h5 mb-0 mr-2"></i>
-            <span>{type}</span>
-            <h4 className="font-weight-bold">{ this.props.value.name }</h4>
+      <div className={`col-sm-6 col-12 mb-4 ${(this.state.active) ? 'col-md-12' : 'col-md-4'}`}>
+        <div className={`project row ${(this.state.active) ? 'opened' : ''}`} onClick={this.open_project}>
+          <div className={this.state.active ? 'col-md-4' : 'col-md-12'}>
+            <div className="image-info position-relative">
+              <img src={ this.props.value.featured_image.url } className="w-100" alt={ this.props.value.name } title={ this.props.value.name } />
+              <div className="info text-light px-4 py-2">
+                <i className="fas fa-desktop h5 mb-0 mr-2"></i>
+                <span>{type}</span>
+                <h4 className="font-weight-bold">{ this.props.value.name }</h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-8 p-5">
+            <h2 className="font-weight-bold">{ this.props.value.name }</h2>
+            <h4 className="text-capitalize">{type}</h4>
+            <h4><Trans i18nKey='Portfolio.company'></Trans>: {this.props.value.company.name}</h4>
+            <div className="my-4">
+              <pre className="h6">{this.props.value.description_PT}</pre>
+            </div>
+            { this.props.value.skills.length > 0 &&
+              <div>
+                <h6><Trans i18nKey='Portfolio.technologies'></Trans></h6>
+                <ul className="list-inline">
+                  { this.props.value.skills.map((value, key) =>
+                    <li key={key} className="list-inline-item font-weight-bold"><i className="fas fa-check mr-2"></i>{value.name}</li>
+                  )}
+                </ul>
+              </div>
+            }
           </div>
         </div>
       </div>
@@ -46,7 +78,7 @@ class Portfolio extends React.Component {
   };
 
   componentDidMount() {
-    this.callApi('/project')
+    this.callApi('/project?_limit=9')
       .then(res => this.setState({ projects: res }))
       .catch(err => console.log(err));
 
@@ -76,7 +108,7 @@ class Portfolio extends React.Component {
             </div>
           </div>
           <div className="row">
-            { this.state.projects.map(value => <Item key={value.toString()} value={value} />)}
+            { this.state.projects.map((value, key) => <Item key={key.toString()} value={value} />)}
           </div>
           { this.state.projects.length < this.state.total_projects &&
             <div className="row mt-5">
